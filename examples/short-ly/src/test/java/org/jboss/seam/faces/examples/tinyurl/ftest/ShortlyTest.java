4be2269b6d863fe77bfe9d808c9388fe668825c1
Marek Schmidt <maschmid@redhat.com>
2011-02-25 10:12:46 +0100

    ftest for short.ly faces example

--- examples/short-ly/src/test/java/org/jboss/seam/faces/examples/tinyurl/ftest/ShortlyTest.java
+++ examples/short-ly/src/test/java/org/jboss/seam/faces/examples/tinyurl/ftest/ShortlyTest.java
@@ -25,6 +25,7 @@ import static org.testng.Assert.assertEquals;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
+import com.thoughtworks.selenium.SeleniumException;
 import java.net.URL;
 import org.jboss.test.selenium.AbstractTestCase;
 import org.jboss.test.selenium.locator.XpathLocator;
@@ -41,11 +42,13 @@ import static org.jboss.test.selenium.guard.request.RequestTypeGuardFactory.*;
  */
 public class ShortlyTest extends AbstractTestCase {
 
-   protected XpathLocator URL_TEXT = xp("//input[contains(@name,':url')]");
-   protected XpathLocator NAME_TEXT = xp("//input[contains(@name,':name')]");
-   protected XpathLocator CREATE_BUTTON = xp("//input[contains(@value,'Create')]");
+   protected final XpathLocator URL_TEXT = xp("//input[contains(@name,':url')]");
+   protected final XpathLocator NAME_TEXT = xp("//input[contains(@name,':name')]");
+   protected final XpathLocator CREATE_BUTTON = xp("//input[contains(@value,'Create')]");
+   protected final XpathLocator DELETEALL_BUTTON = xp("//input[contains(@value,'deleteAll')]");
 
-   protected AttributeLocator FACES_LINK_HREF = xp("//a[text()=\"faces\"]").getAttribute(HREF);
+   protected final XpathLocator ROOT_LINK = xp("//a[text()=\"root\"]");
+   protected final AttributeLocator ROOT_LINK_HREF = ROOT_LINK.getAttribute(HREF);
 
    @BeforeMethod
    public void openStartUrl() throws MalformedURLException
@@ -55,11 +58,27 @@ public class ShortlyTest extends AbstractTestCase {
    }
  
    @Test
-   public void testCreate()
+   public void testCreate() throws MalformedURLException
    {
-      selenium.type(URL_TEXT, "http://www.seamframework.org/Seam3/FacesModule");
-      selenium.type(NAME_TEXT, "faces");
+      // deleteAll button is not displayed if there are no links
+      assertEquals(selenium.isElementPresent(DELETEALL_BUTTON), false);
+
+      // We can only test pages on the same domain, the only interesting page we can be quite sure to exist on the same domain is the context root
+      selenium.type(URL_TEXT, contextRoot.toString());
+      selenium.type(NAME_TEXT, "root");
       waitHttp(selenium).click(CREATE_BUTTON);
-      assertEquals(selenium.getAttribute(FACES_LINK_HREF), "/short.ly/faces");
+
+      assertEquals(selenium.getAttribute(ROOT_LINK_HREF), "/short.ly/root");
+
+      waitHttp(selenium).click(ROOT_LINK);
+      assertEquals(selenium.getLocation().toString(), contextRoot.toString());
+   }
+
+   @Test(dependsOnMethods={"testCreate"})
+   public void testDeleteAll()
+   {
+      waitHttp(selenium).click(DELETEALL_BUTTON);
+      assertEquals(selenium.isElementPresent(DELETEALL_BUTTON), false);
+      assertEquals(selenium.isElementPresent(ROOT_LINK), false);
    }
 }
